bc595ad8fcab5bc56e05ae6f84a6542dc5cd040d
ijlalWindhi <ijlalwindhi15@gmail.com>
2022-08-10 16:01:40 +0700

    first

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn fizz_buzz(x: i32) {
+    for i in 1..x {
+        if i % 3 == 0 && i % 5 == 0 {
+            println!("FizzBuzz");
+        } else if i % 3 == 0 {
+            println!("Fizz");
+        } else if i % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", i);
+        }
+    }
+}
+fn main() {
+    fizz_buzz(100);
+}
